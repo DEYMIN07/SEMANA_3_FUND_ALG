249e5904a96eceea6890ffb99f727bb71dd8c913
Kevin <Kevin@upn.pe>
2025-04-08 19:13:12 -0500

    termine mi algoritmo

--- EJERCICIO_1/Program.cs
+++ EJERCICIO_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace EJERCICIO_1
     {
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int suma;
+            //Console.ReadKey();
+            Console.Write("ingrese numero 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            // console.readline es igual a leer en pseint
+            Console.Write("ingrese numero 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            suma = num1 + num2;
+            Console.WriteLine("la suma es: " +suma);
+
         }
     }
 }
